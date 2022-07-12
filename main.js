c9969f092c18445601159401077749bee9b2a75a
Armando Grados <josear-08@hotmail.com>
2022-07-11 23:17:14 -0500

    Primera subida de portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Passionate Programer","n Entrepreneur"," Software Developer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    cursorChar: '_',
+    loop: true,
+}) 
